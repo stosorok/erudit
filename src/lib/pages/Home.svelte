20660b15b901f7570126bb29aa3b8ba39dcea608
oxygene4 <5797565@gmail.com>
2023-10-05 10:25:04 +0200

    chore: test routing

---
+++ src/lib/pages/Home.svelte
@@ -0,0 +1,45 @@
+<div class="container">
+  <section>
+    <h1>👋 Erudit Sigafon</h1>
+    <img src="https://i.ibb.co/mRQ00gH/image.png" alt="Сігафон" />
+
+    <div class="center">
+      <a href="pages/info.html"
+        ><button class="button" disabled>Click 1</button></a
+      >
+      <a href="pages/info.html"><button class="button">Click 2</button></a>
+    </div>
+
+    <h2>Опис</h2>
+    <div>
+      Ерудіт сігафон це унікальний пристрій на ринку мобільних телефонів. Він
+      оснащеній блоком який поміщайє в собі 3 сигарети, тактильно приємний
+      затвор для видачі сигарет з вібрацією, а також запальничку у верхній
+      частині корпусу. <br />
+      Цей пристрій підкреслить ваш стиль, та допоможе насолоджуватись тютюном завжди!
+      Зручно та елегантно!
+    </div>
+  </section>
+
+  <section>
+    <video
+      src="https://matrats.tv/images/video/0/OrtoLite145.mp4"
+      autoplay
+      muted
+    />
+  </section>
+
+  <section>
+    <input type="text" name="username" placeholder="Введіть ваш логін" />
+    <input type="password" name="password" placeholder="Введіть ваш пароль" />
+
+    <button type="button">
+      <a href="/info">жмать сюда</a>
+    </button>
+
+    <input type="color" />
+    <div class="center">
+      <a href="www.erudit.agency">Erudit Web Studio &copy;</a>
+    </div>
+  </section>
+</div>
---
+++ src/lib/pages/Info.svelte
@@ -0,0 +1,37 @@
+<div class="gap">
+  <div class="cards cards-center">
+    <div class="card">
+      <p>1</p>
+    </div>
+    <div class="card">
+      <p>2</p>
+    </div>
+    <div class="card">
+      <p>3</p>
+    </div>
+  </div>
+
+  <div class="cards cards-between">
+    <div class="card">
+      <p>1</p>
+    </div>
+    <div class="card">
+      <p>2</p>
+    </div>
+    <div class="card">
+      <p>3</p>
+    </div>
+  </div>
+
+  <div class="cards cards-around">
+    <div class="card">
+      <p>1</p>
+    </div>
+    <div class="card">
+      <p>2</p>
+    </div>
+    <div class="card">
+      <p>3</p>
+    </div>
+  </div>
+</div>
